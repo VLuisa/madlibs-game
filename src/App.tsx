22d49df733a4a3a63c91c259a2b8fa5b1ffb2636
Luisa Vasquez <luna.v97@gmail.com>
2025-01-14 14:29:52 -0500

    fill in madlib with random verbs and nouns

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import { useState, useEffect } from "react";
 import OpenAI from "openai";
 
+const verbs = ["run", "code", "eat", "sleep", "knit", "dance"];
+const nouns = ["dog", "cup", "computer", "beanie", "train", "apple"];
+
+function getRandomInt(max) {
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * maxFloored); // The maximum is exclusive and the minimum is inclusive
+}
+
+const getContentFromLine = (line) => {
+  if (line.text) {
+    return line.text;
+  }
+  const isVerb = line.blank === "verb";
+  if (isVerb) {
+    const verbIndex = getRandomInt(verbs.length);
+    console.log("verb index", verbIndex);
+    return <b>{verbs[verbIndex]}</b>;
+  }
+  const nounIndex = getRandomInt(verbs.length);
+  console.log("noun index", nounIndex);
+  return <b>{nouns[getRandomInt(nouns.length)]}</b>;
+};
+
 const App = () => {
-  const [content, setContent] = useState<string>("");
+  const [content, setContent] = useState([]);
 
   useEffect(() => {
     const fetchDataAsync = async () => {
@@ -23,10 +46,12 @@ const App = () => {
             },
           ],
         });
-        const contentResponse = completion.choices[0].message.content + "";
+        const contentResponse = completion.choices[0].message.content;
+        const jsonResponse = JSON.parse(contentResponse!);
 
-        console.log("message1", contentResponse);
-        setContent(contentResponse);
+        console.log("message1", typeof JSON.parse(contentResponse!));
+        console.log("object", jsonResponse);
+        setContent(jsonResponse);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -35,7 +60,7 @@ const App = () => {
     fetchDataAsync();
   }, []);
 
-  return <div>{content}</div>;
+  return <div>{content.map((line, index) => getContentFromLine(line))}</div>;
 };
 
 export default App;
