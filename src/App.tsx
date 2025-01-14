70af216aafd9bb33f9c6b203b83771d46b4d75cd
Brad Dettmer <bd@MacBook-Pro-Bradley.local>
2025-01-14 15:01:56 -0500

    make story geeky, and render noun and verb count to page

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,33 @@ import OpenAI from "openai";
 const verbs = ["run", "code", "eat", "sleep", "knit", "dance"];
 const nouns = ["dog", "cup", "computer", "beanie", "train", "apple"];
 
-function getRandomInt(max) {
+function countInstances(arr: Array<any>, key: string): number {
+  const result = arr.filter((item) => 'blank' in item && item.blank === key);
+  return result.length
+}
+
+function getRandomInt(max: number) {
   const maxFloored = Math.floor(max);
   return Math.floor(Math.random() * maxFloored); // The maximum is exclusive and the minimum is inclusive
 }
 
-const getContentFromLine = (line) => {
+const getFieldsFromLine = (line) => {
   if (line.text) {
     return line.text;
   }
   const isVerb = line.blank === "verb";
   if (isVerb) {
     const verbIndex = getRandomInt(verbs.length);
-    console.log("verb index", verbIndex);
     return <b>{verbs[verbIndex]}</b>;
   }
   const nounIndex = getRandomInt(verbs.length);
-  console.log("noun index", nounIndex);
-  return <b>{nouns[getRandomInt(nouns.length)]}</b>;
+  return <b>{nouns[nounIndex]}</b>;
 };
 
 const App = () => {
   const [content, setContent] = useState([]);
+  const [verbCount, setVerbCount] = useState(0);
+  const [nounCount, setNounCount] = useState(0);
 
   useEffect(() => {
     const fetchDataAsync = async () => {
@@ -42,7 +47,7 @@ const App = () => {
             {
               role: "user",
               content:
-                "Write a madlibs style story. Limit parts of speech of blanks to nouns and verbs. Return it in the following format: [{text: 'The quick brown fox'},{blank: 'verb'},{text: 'over the lazy fence'}]. Only return the JSON of the story not formatted as a code block.",
+                "Write a geeky madlibs style story. Limit parts of speech of blanks to nouns and verbs. Return it in the following format: [{text: 'The quick brown fox'},{blank: 'verb'},{text: 'over the lazy fence'}]. Only return the JSON of the story not formatted as a code block.",
             },
           ],
         });
@@ -52,6 +57,8 @@ const App = () => {
         console.log("message1", typeof JSON.parse(contentResponse!));
         console.log("object", jsonResponse);
         setContent(jsonResponse);
+        setNounCount(countInstances(jsonResponse, 'noun'))
+        setVerbCount(countInstances(jsonResponse, 'verb'))
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -60,7 +67,10 @@ const App = () => {
     fetchDataAsync();
   }, []);
 
-  return <div>{content.map((line, index) => getContentFromLine(line))}</div>;
+  return <div>
+    <p>verb count: {verbCount}</p>
+    <p>noun count: {nounCount}</p>
+    {content.map((line, index) => getFieldsFromLine(line))}</div>;
 };
 
 export default App;
